5f6dee28442beb254364c76943e09042f92e4c71
gingerab <singleplayer176@gmail.com>
2025-04-28 20:33:29 +0200

    added faster render .md -> .pdf script

--- functions/pandla.fish
+++ functions/pandla.fish
@@ -2,19 +2,28 @@ complete -c pandla -e
 complete -c pandla -r -F
 
 function pandla
-    set mdfile $argv[1]
-    set flags $argv[2..-1]
-    
     if test (count $argv) -lt 1
         echo "Usage: pandla yourfile.md [pandoc flags]"
         return 1
     end
 
-    # Convert .md to temporary .latex
-    pandoc $mdfile -o a.latex $flags
+    set mdfile $argv[1]
+    set flags $argv[2..-1]
+    set basename (string replace -r '\.md$' '' (basename -- $mdfile))
+
+    # Convert .md to .latex
+    pandoc $mdfile -o "$basename.latex" $flags
+    if test $status -ne 0
+        echo "Pandoc failed."
+        return 1
+    end
 
     # Compile to PDF
-    latexmk -pdf a.latex -silent -c
-    # Clean up
-    rm a.latex
+    latexmk -pdf -silent "$basename.latex" && latexmk -c "$basename.latex"
+    if test $status -ne 0
+        echo "Latexmk failed."
+        return 1
+    end
+
+    rm "$basename.latex"
 end
